7c40b50df8fde6fe8e11cb65c5a415f55cad020c
Tal Sagi <tals7300@gmail.com>
2022-08-29 18:18:11 +0300

    /moviesPerActor endpoint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 const axios = require('axios').default;
 const express = require('express');
+const { API_KEY } = require('./constants');
+const { movies, actors } = require('../dataForQuestions');
 
 const port = Number(process.env.PORT) || 3000;
 const app = express();
 app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
 
 app.get("/", (req, res) => res.send(sayHey("Marvel enthusiast")))
+app.get("/moviesPerActor", async (req, res) => res.send(await moviesPerActor()));
+
+async function moviesPerActor() {
+    const arrayedResult = await Promise.all(actors.map(moviesOfActorByName));
+
+    return arrayedResult.reduce((acc, moviesOfActor) => ({...acc, ...moviesOfActor}), {});
+}
+
+async function moviesOfActorByName(actorName) {
+    const actorId = await getActorId(actorName);
+    const movieCharacters = await movieCharactersOfActorById(actorId)
+    
+    return {[actorName]: Object.keys(movieCharacters)};
+}
+
+async function getActorId(actorName) {
+    const searchQuery = actorName.replace(/ /g, "+");
+    const searchResponse = await axios.get(`https://api.themoviedb.org/3/search/person?api_key=${API_KEY}&query=${searchQuery}`);
+
+    return searchResponse.data.results[0].id;
+}
+
+async function movieCharactersOfActorById(actorId) {
+    const movieCreditsResponse = await axios.get(`https://api.themoviedb.org/3/person/${actorId}/movie_credits?api_key=${API_KEY}`)
+    const marvelMovieCredits = movieCreditsResponse.data.cast.filter(movieCredit => Object.values(movies).includes(movieCredit.id));
+    const movieCharacters = marvelMovieCredits.reduce((acc, movieCredit) => ({...acc, [movieCredit.title]: movieCredit.character}), {});
+    
+    return movieCharacters;
+}
 
 const sayHey = (name) => {
     return `Hey ${name}`
